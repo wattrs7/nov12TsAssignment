7cdb61b15395563dd10afadfbc7125c947504020
Rob Watt <wattrs7@yahoo.com>
2024-11-12 12:38:58 -0500

    Initialo commit: Project set up with Header component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main>
+        <h2>Welcome to my blog!</h2>
+        <p>This is my first React component.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+      <header className="blog-header">
+        <h1>My Awesome Blog</h1>
+        <nav>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+  
+  export default Header;
+  
